dfafefa752290d7295ca68be4b6b0cf5e7797509
ArtemFadin2023 <vipartemfadin@gmail.com>
2023-10-21 10:55:28 +0300

    добавленно решение

---
+++ GitHub.py
@@ -0,0 +1,10 @@
+class Alphabet:
+ def init(self, lang, letters):
+  self.lang = lang
+  self.letters = letters
+
+def print(self):
+    print("Alphabet: ", self.letters)
+
+def letters_num(self):
+    return len(self.letters)
